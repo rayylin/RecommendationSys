643079882f4cb3fdc424e1a176bd54595bbf7cb3
rayl <dwadeflashback@gmail.com>
2024-11-29 01:38:11 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+import pyodbc
+
+print(pyodbc.drivers())
+
+app = Flask(__name__)
+
+# Database connection using Windows Authentication
+conn = pyodbc.connect(
+    'DRIVER={ODBC Driver 17 for SQL Server};'
+    'SERVER=ray\SQLEXPRESS;'
+    'DATABASE=RecSys;'
+    'Trusted_Connection=yes;'
+)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    cursor = conn.cursor()
+
+    # Fetch categories
+    cursor.execute("SELECT CategoryID, CategoryName FROM ProductCategories")
+    categories = cursor.fetchall()
+
+    selected_category = request.form.get('category')
+    products = []
+
+    if selected_category:
+        cursor.execute(
+            "SELECT ProductName, ImageSource FROM Products WHERE Category = ?",
+            selected_category,
+        )
+        products = cursor.fetchall()
+
+    return render_template('index.html', categories=categories, products=products)
+
+if __name__ == '__main__':
+    app.run(debug=True)
