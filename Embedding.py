6a3d8ab49c0da5d5d8f872e2eba539efcc25d2d2
rayl <dwadeflashback@gmail.com>
2025-02-01 23:46:07 -0500

    add store information

--- Embedding.py
+++ Embedding.py
@@ -70,13 +70,17 @@ docs = create_faiss_vector_store(file_path1)
 
 
 
-docs = [
-"Clothing (Aisle 1): [T-shirt,Jeans,Jacket,Sneakers,Hat,Dress,Socks,Gloves,Scarf,Sweater,Shorts,Belt,Coat,Sandals]"
+docs = [{"Chatham":["Clothing (Aisle 1): [T-shirt,Jeans,Jacket,Sneakers,Hat,Dress,Socks,Gloves,Scarf,Sweater,Shorts,Belt,Coat,Sandals]"
 ,"Electronics (Aisle 4): [cellphone,Laptop,Tablet,Headphones,Camera,Television,Smartwatch,Speaker,Drone,Gaming Console,Printer,Keyboard]"
 ,"Food (Aisle 5): Apple,Banana,Orange,Bread,Milk,Cheese,Yogurt,Chicken"
 ,"Furniture (Aisle 3): Chair,Desk,Sofa,Bed,Coffee Table,Wardrobe,Bookshelf,Dining Table,Nightstand,Dresser"
-,"Personal Care (Aisle 2): Shampoo,Soap,Toothpaste,Deodorant,Face Wash,Lotion"
-]
+,"Personal Care (Aisle 2): Shampoo,Soap,Toothpaste,Deodorant,Face Wash,Lotion"],
+"Queens":["Clothing (Aisle 3): [T-shirt,Jeans,Jacket,Sneakers,Hat,Dress,Socks,Gloves,Scarf,Sweater,Shorts,Belt,Coat,Sandals]"
+,"Electronics (Aisle 5): [cellphone,Laptop,Tablet,Headphones,Camera,Television,Smartwatch,Speaker,Drone,Gaming Console,Printer,Keyboard]"
+,"Food (Aisle 7): Apple,Banana,Orange,Bread,Milk,Cheese,Yogurt,Chicken"
+,"Furniture (Aisle 2): Chair,Desk,Sofa,Bed,Coffee Table,Wardrobe,Bookshelf,Dining Table,Nightstand,Dresser"
+,"Personal Care (Aisle 6): Shampoo,Soap,Toothpaste,Deodorant,Face Wash,Lotion"]
+}]
 
 # Convert raw document texts to LangChain Document objects
 documents = [Document(page_content=doc) for doc in docs]
